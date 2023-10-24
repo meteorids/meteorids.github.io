a7a79cef08bc2f77553280d82ff57b9cecc5a0f4
rajmundtakacs <rajmundtakacs@gmail.com>
2023-10-24 15:27:37 +0200

    Refresh

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a portfolio project task from codecademy.
+The site shows, how could I imagine my future portfolio site.
--- script.js
+++ script.js
@@ -1,77 +1,15 @@
-// Actual year in footer
+const ball = document.getElementById('circle');
 
-document.getElementById('year').innerHTML = new Date().getFullYear();
-
-// Changing button's text by hovering on contact site
-
-let emailButton = document.getElementById('email');
-
-function changeEmailText() {
-    emailButton.innerHTML = 'milostipanov@gmail.com';
+function switchOn() {
+  ball.style.borderRadius = '0px';
+  ball.removeEventListener('click', switchOn);
+  ball.addEventListener('click', switchOff);
 }
 
-function backEmailText() {
-    emailButton.innerHTML = 'e-mail'; 
-} 
-
-emailButton.addEventListener('mouseover', changeEmailText);
-
-emailButton.addEventListener('mouseout', backEmailText);
-
-
-let githubButton = document.getElementById('github');
-
-function changeGithubText() {
-    githubButton.innerHTML = 'github.com/meteorids';
+function switchOff() {
+  ball.style.borderRadius = '100px';
+  ball.removeEventListener('click', switchOff);
+  ball.addEventListener('click', switchOn);
 }
 
-function backGithubText() {
-    githubButton.innerHTML = 'github'; 
-} 
-
-githubButton.addEventListener('mouseover', changeGithubText);
-
-githubButton.addEventListener('mouseout', backGithubText);
-
-
-let linkedinButton = document.getElementById('linkedin');
-
-function changeLinkedinText() {
-    linkedinButton.innerHTML = 'Dr. Milos Stipanov';
-}
-
-function backLinkedinText() {
-    linkedinButton.innerHTML = 'linkdin'; 
-} 
-
-linkedinButton.addEventListener('mouseover', changeLinkedinText);
-
-linkedinButton.addEventListener('mouseout', backLinkedinText);
-
-
-let researchgateButton = document.getElementById('researchgate');
-
-function changeResearchgateText() {
-    researchgateButton.innerHTML = 'Dr. Milos Stipanov';
-}
-
-function backResearchgateText() {
-    researchgateButton.innerHTML = 'Research Gate'; 
-} 
-
-researchgateButton.addEventListener('mouseover', changeResearchgateText);
-
-researchgateButton.addEventListener('mouseout', backResearchgateText);
-
-
-// Toggle navbar
-
-
-function toggleNav() {
-    let x = document.getElementById("mobilelinks");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
-  } 
+ball.addEventListener('click', switchOn);

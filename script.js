cd8ccaeedd983315f358ddb8deed259b342fee06
mapadu <rajmundtakacs@gmail.com>
2023-11-02 13:24:57 +0100

    Fix

--- script.js
+++ script.js
@@ -3,17 +3,6 @@
 document.getElementById('year').innerHTML = new Date().getFullYear();
 
 
-// Changing button's text by hovering on contact site
-
-/*
-const buttons = document.getElementsByClassName("button");
-
-for (const elem of buttons) {
-    const origText = elem.innerHTML;
-    elem.addEventListener("mouseover", () => elem.innerHTML = elem.dataset.hovertext);
-    elem.addEventListener("mouseout", () => elem.innerHTML = origText);
-}
-*/
 // Toggle navbar
 
 
